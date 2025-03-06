6ca8f938a85faed4ebdb88b35f2399274ecec2f8
me-cafebabe <me.cafebabe@gmail.com>
2025-03-07 03:10:38 +0500

    akershus: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- device.mk
+++ device.mk
@@ -430,7 +430,9 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb.gadget-service.qti \
+    usb_compositions.conf
 
 # VNDK
 PRODUCT_PACKAGES += \
