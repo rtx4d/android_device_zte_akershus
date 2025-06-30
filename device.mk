9c6a48c715fcbaa346852616797d01f632594788
Łukasz Patron <priv.luk@gmail.com>
2025-07-01 05:27:47 +0700

    akershus: Build android.hardware.bluetooth@1.0
    Required by libantradio.
    
    Change-Id: I5e26a4154cf54e64f424cb6cf20db87b7023f1cd

--- device.mk
+++ device.mk
@@ -143,6 +143,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl:32 \
     audio.bluetooth.default \
