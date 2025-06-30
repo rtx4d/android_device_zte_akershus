a0ff764f5d1406e395918445f83710cc82c6bd5a
Łukasz Patron <priv.luk@gmail.com>
2025-07-01 05:28:31 +0700

    akershus: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- device.mk
+++ device.mk
@@ -230,7 +230,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # fastbootd
 PRODUCT_PACKAGES += \
