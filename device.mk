c2e9c3bc0fc95d07d9462402e74d7b900e29877c
Bruno Martins <bgcngm@gmail.com>
2025-07-01 05:47:31 +0700

    akershus: Remove deprecated trust HAL
    Change-Id: Ic5ac79b4ca60b87a97622877b97f806c07cab907

--- device.mk
+++ device.mk
@@ -445,10 +445,6 @@ PRODUCT_PACKAGES += \
 # Treble
 PRODUCT_USE_VNDK_OVERRIDE := true
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
