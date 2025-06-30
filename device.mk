45dae99f113cade125ef132b00211cda7b2f6668
Michael Bestas <mkbestas@lineageos.org>
2025-07-01 05:47:56 +0700

    akershus: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618

--- device.mk
+++ device.mk
@@ -456,7 +456,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # VNDK
 PRODUCT_COPY_FILES += \
