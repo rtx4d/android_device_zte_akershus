1599ecb6cb206bdb08520ccc8de41ffe8d693e78
Nolen Johnson <johnsonnolen@gmail.com>
2025-07-05 20:26:29 +0700

    akershus: Cleanup dead targets
    Offending entries:
    android.hardware.gnss@2.1-service-qti
    build/make/core/main.mk:1308: error: Build failed.
    
    Change-Id: I0e4d93f4c3be077d49befa1f547d11d623ca74d5

--- device.mk
+++ device.mk
@@ -247,8 +247,7 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.1.vendor \
-    android.hardware.gnss@2.1-service-qti
+    android.hardware.gnss@2.1.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
