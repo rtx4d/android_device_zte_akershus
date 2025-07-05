27cc0b2075e8f925ae13d64eefa80b19f3b9e3c4
Michael Bestas <mkbestas@lineageos.org>
2025-07-05 20:28:57 +0700

    akershus: Drop COMMON_LUNCH_CHOICES
    As of 14 QPR2 this is broken, let's remove it instead of updating it
    every 3 months. breakfast is the way to go.
    
    Change-Id: I097bd54b894d9a49c7ce8b1ab6f343349e268fd8

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,8 @@
 #
-# Copyright (C) 2024 The LineageOS Project
+# Copyright (C) 2024-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_akershus.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_akershus-user \
-    lineage_akershus-userdebug \
-    lineage_akershus-eng
