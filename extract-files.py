8a622e77d3992e82febe10c86decf03911b7a323
Michael Bestas <mkbestas@lineageos.org>
2025-07-05 21:06:46 +0700

    akershus: Switch to python extract-utils
    Change-Id: Ia13b728464495ff208ca1a706847b11af5c93ae8

---
+++ extract-files.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import extract_utils.tools
+
+extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_9'
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    'vendor/lib/libZEffectLib.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+    'vendor/bin/hw/vendor.qti.hardware.sensorscalibrate@1.0-service': blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
+    ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'akershus',
+    'zte',
+    blob_fixups=blob_fixups,
+    check_elf=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
