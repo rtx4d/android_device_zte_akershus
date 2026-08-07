1bc9545e120a8bfa79409ab090e0b99dcfe01e05
Bruno Martins <bgcngm@gmail.com>
2026-08-07 21:20:21 +0700

    akershus: livedisplay: Migrate to AIDL
    Change-Id: I700115d012d9db96333ce69a4eb0be1f6c456c1a

--- device.mk
+++ device.mk
@@ -292,7 +292,9 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay-service.sdm
+
+$(call soong_config_set,livedisplay_sdm,enable_pa,false)
 
 # Media
 PRODUCT_PACKAGES += \
