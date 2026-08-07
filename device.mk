f7bb38694dc535e9379efa5f3db12f9218920233
Nolen Johnson <johnsonnolen@gmail.com>
2026-08-07 21:15:57 +0700

    akershus: Stop copying libhidlbase-v32
    Change-Id: I5073c64e48f6b86be3627b81020e2a0ea39b3c54

--- device.mk
+++ device.mk
@@ -445,9 +445,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstdc++_vendor
 
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so 
-
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
